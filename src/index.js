1b0bc8359b82ac647a0fd9bf95eb7a7410dfd5dc
omarnabi1 <onabi20@gmail.com>
2020-10-10 13:52:15 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  
+  const taskForm = document.getElementById('create-task-form');
+  const task = document.getElementById('tasks');
+ 
+  taskForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const newTask = document.getElementById('new-task-description').value;
+
+    task.innerHTML += `<li>${newTask}<button data-action = "delete"> X </button></li>`;
+    
+    taskForm.reset() 
+  })
+
+  task.addEventListener("click", function(event){
+    if (event.target.dataset.action === "delete"){
+      event.target.parentElement.remove();
+    }
+  })
+
 });
